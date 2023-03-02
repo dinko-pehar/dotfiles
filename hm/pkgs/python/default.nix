a97771e51142a96496ec5b446d6c7e6d6f814070
Dinko Pehar <dinko@pehar.dev>
2023-03-02 16:10:32 +0100

    Add vulture and black

--- hm/pkgs/python/default.nix
+++ hm/pkgs/python/default.nix
@@ -8,9 +8,10 @@
     #python312
     poetry # Python Env management
     python310Packages.bandit # Static Analyzer of Python code
+    python310Packages.vulture
+    python310Packages.black
     python310Packages.isort
     python310Packages.flake8
-    python310Packages.six
   ];
 
   home.file.".config/pip/pip.conf".text = builtins.readFile ./pip.conf;
