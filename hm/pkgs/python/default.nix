ed2121820302ab712f669ae76e526e0a8d3b0ebc
Dinko Pehar <dinko@pehar.dev>
2023-03-07 00:47:18 +0100

    Override poetry for fish completions

--- hm/pkgs/python/default.nix
+++ hm/pkgs/python/default.nix
@@ -6,12 +6,22 @@
     python310
     #python311
     #python312
-    poetry # Python Env management
+    # NOTE: This applies correct completion for fish shell
+    (poetry.overrideAttrs (oldAttrs: {
+      postInstall = ''
+        installShellCompletion --cmd poetry \
+      --bash <($out/bin/poetry completions bash) \
+      --fish <($out/bin/poetry completions fish | head -n 80) \
+      --zsh <($out/bin/poetry completions zsh) \
+      '';
+    })) # Python Env management
     python310Packages.bandit # Static Analyzer of Python code
     python310Packages.vulture
     python310Packages.black
     python310Packages.isort
     python310Packages.flake8
+    # python310Packages.debugpy
+    python310Packages.pudb
   ];
 
   home.file.".config/pip/pip.conf".text = builtins.readFile ./pip.conf;
